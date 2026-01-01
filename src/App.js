4e4d5be302cd686263af54083923fc50da94fa8c
TalhaKhanSix <talhakhansix@gmail.com>
2026-01-01 20:30:01 +0500

    starting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import "./App.css";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="portfolio">
-      <Header />
-      <main>
-        <Hero />
-        <About />
-        <Skills />
-        <Projects />
-        <Contact />
-      </main>
-      <Footer />
+    <div className="App">
+      <h1>Hello World</h1>
     </div>
   );
 }
